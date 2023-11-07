738c8bd5046b418de24502aae0f777c3f7a314c4
Henry Seigbein <hensco95@gmail.com>
2023-11-07 19:40:15 +0100

    set up tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,28 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    theme: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+    },
+    extend: {
+      colors: {
+        red: "hsl(0, 87 %, 67 %)",
+        cyan: "hsl(180, 66%, 49%)",
+        darkViolet: "hsl(257, 27%, 26%)",
+        gray: "hsl(0, 0%, 75%)",
+        grayishViolet: "hsl(257, 7%, 63%)",
+        veryDarkBlue: "hsl(255, 11%, 22%)",
+        veryDarkViolet: "hsl(260, 8%, 14%)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
-
