399b5d44df8f9a2a565fd6653e8d189472da62d3
Henry Seigbein <hensco95@gmail.com>
2023-11-15 02:25:03 +0100

    finished grid section and boost section

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
-// let msg = document.querySelector(".error-p");
+let input = document.getElementById("input_id");
+let link_form = document.getElementById("link-form");
+let err_msg = document.getElementById("error-msg");
 
-// let submit_btn = document.getElementById("#shorten");
+link_form.addEventListener("submit", formSubmit);
 
-// let input_value = document.querySelector(".input-value");
+// Function to validate a URL
+function validURL(str) {
+  var pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+      "((\\d{1,3}\\.){3}\\d{1,3}))" +
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+      "(\\?[;&a-z\\d%_.~+=-]*)?" +
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  );
+  return !!pattern.test(str);
+}
 
+function formSubmit(e) {
+  e.preventDefault();
 
+  if (input.value === "") {
+    err_msg.innerHTML = "Please input a value";
+    input.classList.add("border-red");
+    setTimeout(() => {
+      err_msg.innerHTML = "";
+      input.classList.remove("border-red");
+    }, 3000);
+  } else if (!validURL(input.value)) {
+    err_msg.innerHTML = "Please input a valid URL";
+    input.classList.add("border-red");
+    setTimeout(() => {
+      err_msg.innerHTML = "";
+      input.classList.remove("border-red");
+    }, 3000);
+  } else {
+    err_msg.innerHTML = "";
+    input.classList.remove("border-red");
+    alert(input.value)
+  }
+}
